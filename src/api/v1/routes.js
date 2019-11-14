1f376ae30d3f79027def0b7d3beaecdfdd120826
gle <gle@certi.org.br>
2019-11-14 20:41:39 -0300

    [Fix] Add query string validation

--- src/api/v1/routes.js
+++ src/api/v1/routes.js
@@ -1,9 +1,83 @@
 const express = require('express');
+const lodash = require('lodash');
 const mockData = require('../libs/mockData');
 const numberFormat = require('../libs/numberFormat');
 
 const apiRootPath = '/api/v1';
 const router = express.Router()
+
+const defaultParams = [
+  'startTimestamp',
+  'endTimestamp',
+  'minuteStep',
+  'minPower',
+  'maxPower',
+]
+
+/**
+ * Function that receives the request object
+ * and retrieve only the necessary API parameters.
+ * @param {Object} req 
+ * @returns {Object} queryParams
+ */
+const getQueryParams = (req) => {
+  let queryParams = {};
+
+  Object.getOwnPropertyNames(req).forEach(
+    attribute => {
+      let value = defaultParams.filter(element => element === attribute)[0]
+        ? req[attribute]
+        : '';
+      if (value !== '') {
+        lodash.set(queryParams, attribute, value);
+      }
+    }
+  )
+
+  return queryParams;
+};
+
+/**
+ * Function that takes the parsed query string paramters,
+ * validate its content, and return a array containing
+ * messages for each format violation committed.
+ * @param {Object} queryParams 
+ * @returns {Array} messages
+ */
+function validateQueryParams(queryParams) {
+  let messages = [];
+
+  if (queryParams === {}) {
+    console.log('passou aqui');
+    return [{ queryParams: 'You must specify the APi parameters in a query string format.' }];
+  }
+
+  Object.getOwnPropertyNames(queryParams).forEach(
+    attribute => {
+      switch(attribute) {
+        case 'startTimestamp':
+          if (!queryParams[attribute]) {
+            messages.push({ attribute: 'You must also specify endTimestamp.' });
+          };
+          break;
+        case 'endTimestamp':
+          if (!queryParams[attribute]) {
+            messages.push({ attribute: 'You must also specify startTimestamp.' });
+          }
+          break;
+        case 'minuteStep':
+          if (queryParams[attribute] && !numberFormat.isPositiveInt(queryParams[attribute])) {
+            messages.push({ attribute: 'The minute step must be a positive integer.' });
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  );
+
+  return messages;
+};
 
 // Middleware that logs the requests made to the API
 router.use(function requestLog(req, res, next) {
@@ -12,33 +86,31 @@ router.use(function requestLog(req, res, next) {
 });
 
 router.get(`${apiRootPath}/consumption/**`, (req, res) => {
-  let startTimestamp = req.query.startTimestamp;
-  let endTimestamp = req.query.endTimestamp;
-  let minuteStep = req.query.minuteStep;
-  let minPower = req.query.minPower;
-  let maxPower = req.query.maxPower;
+  let queryParams = getQueryParams(req.query);
+  let badRequestMessages = validateQueryParams(queryParams);
+  console.log(badRequestMessages);
 
-  let startDt = new Date(startTimestamp);
-  let endDt  = new Date(endTimestamp);
+  if (badRequestMessages === []) {
+    res.status(400).send({ messages: badRequestMessages });
+  }
+
+  let startDt = new Date(queryParams.startTimestamp);
+  let endDt  = new Date(queryParams.endTimestamp);
 
   if (endDt.getTime() - startDt.getTime() < 0) {
     res.status(400).send({ message: 'Invalid period (startTimestamp > endTimestamp).'})
   }
 
-  if (!numberFormat.isPositiveInt(minuteStep)) {
-    res.status(400).send({ message: 'The minute step must be a positive integer.'})
-  }
-
-  if (maxPower - minPower <= 0) {
+  if (queryParams.maxPower - queryParams.minPower <= 0) {
     res.status(400).send({ message: 'The maximum power must be greater than the minimum power.'})
   }
 
   const periodSeries = mockData.generatePowerTimeSeries(
-    startTimestamp,
-    endTimestamp,
-    minuteStep,
-    minPower,
-    maxPower
+    queryParams.startTimestamp,
+    queryParams.endTimestamp,
+    queryParams.minuteStep,
+    queryParams.minPower,
+    queryParams.maxPower
   );
 
   res.status(200).send(periodSeries);
