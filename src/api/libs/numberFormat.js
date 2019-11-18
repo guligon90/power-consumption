4977e619bc14bd9be39a87fee8fec3734e9504ea
gle <gle@certi.org.br>
2019-11-18 19:51:06 -0300

    Feat: Add numeric validation function

--- src/api/libs/numberFormat.js
+++ src/api/libs/numberFormat.js
@@ -2,11 +2,16 @@ const roundToDecimals = (value, decimals = process.env.DEFAULT_DECIMAL_DIGITS) =
   return Number(value.toFixed(decimals));
 };
 
+const isNumeric = (number) => {
+  return !isNaN(parseFloat(number)) && isFinite(number);
+};
+
 const isPositiveInt = (number) => {
   return !isNaN(parseInt(number)) && isFinite(number) && parseInt(number) > 0;
 };
 
 module.exports = {
+  isNumeric,
   isPositiveInt,
   roundToDecimals
 };
